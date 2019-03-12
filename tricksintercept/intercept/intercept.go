b4a9ca6c61aa2afcc55e600b3e624a7f62516e4e
luozhihong <luozhihong@chongdingdahui.com>
2019-03-12 11:43:52 +0800

    add

--- tricksintercept/intercept/intercept.go
+++ tricksintercept/intercept/intercept.go
@@ -7,24 +7,21 @@ import (
 )
 
 type Intercept struct {
-	
 }
-
-var FF = func() {}
 
 var cacheMap = make(map[string]interface{})
 var DataEmpty = errors.New("数据为空")
 
-func (i Intercept) Cache(key string,fi interface{},params ...interface{}) (interface{},error) {
+func (i Intercept) Cache(key string, fi interface{}, params ...interface{}) (interface{}, error) {
 	f := reflect.ValueOf(fi)
-	result,ok := cacheMap[key]
+	result, ok := cacheMap[key]
 	if ok {
 		fmt.Println("from cache get")
-		return result,nil
+		return result, nil
 	}
 
 	in := make([]reflect.Value, len(params))
-	for k,p := range params {
+	for k, p := range params {
 		in[k] = reflect.ValueOf(p)
 	}
 
@@ -32,14 +29,14 @@ func (i Intercept) Cache(key string,fi interface{},params ...interface{}) (inter
 
 	if vs[1].Interface() != nil {
 		fmt.Println("from db get")
-		return nil,vs[1].Interface().(error)
+		return nil, vs[1].Interface().(error)
 	}
 
 	result = vs[0].Interface()
-	if result==nil {
-		return nil,DataEmpty
+	if result == nil {
+		return nil, DataEmpty
 	}
 	fmt.Println("save to db")
 	cacheMap[key] = result
-	return result,nil
+	return result, nil
 }
