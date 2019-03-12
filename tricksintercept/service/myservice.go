665e67fe45ee398bdd71c91e25b0480de31d6d61
luozhihong <luozhihong@chongdingdahui.com>
2019-03-12 12:19:16 +0800

    add

--- tricksintercept/service/myservice.go
+++ tricksintercept/service/myservice.go
@@ -6,32 +6,32 @@ import (
 	"fmt"
 )
 
-func MyTe()  {
-	inter := intercept.Intercept{}
-	r,err := inter.Cache("testkey", hh,1,3)
-	if err!= nil {
+func MyTe() {
+	c := intercept.Intercept{}
+	r, err := c.Cache("testkey", hh, 1, 3)
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	u := r.(entity.User)
+	fmt.Println(u.UserName)
 
-	fmt.Println(r)
-
-	r,err = inter.Cache("testkey", hh,1,3,2)
-	if err!= nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(r)
-
-	r,err = inter.Cache("testkey", hh,1,3,4)
-	if err!= nil {
+	r, err = c.Cache("testkey", hh, 1, 3, 2)
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println(r)
 
-	r,err = inter.Cache("testkey", hh,1,3,4,32)
-	if err!= nil {
+	r, err = c.Cache("testkey", hh, 1, 3, 4)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(r)
+
+	r, err = c.Cache("testkey", hh, 1, 3, 4, 32)
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
@@ -39,6 +39,6 @@ func MyTe()  {
 
 }
 
-func hh(age ,userId int) (entity.User,error)  {
-	return entity.User{UserName:"ruo",UserId:userId,Age:age},nil
+func hh(age, userId int) (entity.User, error) {
+	return entity.User{UserName: "ruo", UserId: userId, Age: age}, nil
 }
